04b09156d58f0d070b5d41b9cc9fffba76936b1b
robtrivi <robtrivi@espol.edu.ec>
2024-07-10 09:57:42 -0500

    Guia 15 completada

--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,8 @@
 
  router.post('/items', itemController.createItem);
  router.get('/items', itemController.getAllItems);
-
+ router.get('/items/:id', itemController.getItem);
+ router.put('/items/:id', itemController.updateItem);
+ router.delete('/items/:id', itemController.deleteItem);
+ 
  module.exports = router;
