bca96cc3de930420a36eb6b10fcf0fb1c5358a97
kevinMaga <118130742+kevinMaga@users.noreply.github.com>
2024-08-19 16:40:56 +0200

    modificacion authenticate_token.js

--- middleware/authenticate_token.js
+++ middleware/authenticate_token.js
@@ -1,28 +1,24 @@
-require('dotenv').config();
 const jwt = require('jsonwebtoken');
-
-const TOKEN_SECRET = process.env.TOKEN_SECRET
+const { TOKEN_SECRET } = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers['token'] 
+    const authHeader = req.headers['token'];
+    
+    console.log(authHeader);
 
-  console.log('Token recibido:', token);
-
-  if (!token) {
-    console.log('Token no encontrado en el requerimiento');
-    return res.status(408).json({ message: 'Token no encontrado en el requerimiento' });
-  }
-
-  jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      console.log('Error de verificación del token:', err);
-      return res.status(408).json({ message: 'Token inválido' });
+    if (!authHeader) {
+        return res.status(408).json({ error: 'Token is missing' });
     }
 
-    console.log('Token decodificado:', decoded);
-    req.user = decoded;
-    next();
-  });
+    
+    jwt.verify(authHeader, TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.status(408).json({ error: 'Invalid token' });
+        }
+
+        req.user = user;
+        next();
+    });
 };
 
 module.exports = verifyToken;
